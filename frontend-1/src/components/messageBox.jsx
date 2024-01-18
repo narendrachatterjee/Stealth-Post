115bcd3d4b8a1bc68ae56352334b192d304f8ab5
Narendra Chatterjee <48941364+narendrachatterjee@users.noreply.github.com>
2024-01-19 00:24:12 +0530

    Delete frontend-1 directory

--- frontend-1/src/components/messageBox.jsx
+++
@@ -1,7 +0,0 @@
-const MessageBox = () =>{
-    return(
-        <div className="">Message Box</div>
-    )
-}
-
-export default MessageBox;
--- frontend-1/src/utils/config.js
+++
@@ -1,1 +0,0 @@
-export const backendUrl = "https://stealthpost.onrender.com";  
